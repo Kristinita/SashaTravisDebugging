c446a4e9a8e6969f5123bae62d812df5fcfcfe85
Kristinita <Kristinita@users.noreply.github.com>
2025-10-24 19:28:26 +0300

    test(pa11y): try running pa11y with the .js config file
    Signed-off-by: Kristinita <Kristinita@users.noreply.github.com>

--- .pa11yci.js
+++ .pa11yci.js
@@ -1,15 +1,10 @@
 module.exports = {
 
 	defaults: {
-
-		/* [INFO] Launch options for the headless Chrome:
-		https://github.com/pa11y/pa11y#chromelaunchconfig-object
-		https://pptr.dev/api/puppeteer.launchoptions */
 		chromeLaunchConfig: {
 			args: [
 				"--no-sandbox"
 			]
-			// headless: "shell"
 		},
 	}
 };
